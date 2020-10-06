8e471ffc912e42f563e1e3e46470cd8bc132de0a
Tyler Jones <tejones1102@gmail.com>
2020-10-05 21:33:08 -0700

    Updated Font.

--- client/component/ArtForm.jsx
+++ client/component/ArtForm.jsx
@@ -3,19 +3,22 @@ import styles from './styles.css';
 
 
 const ArtForm = (props) => (
-  <form>
-    Title: <input type="text" name="title" value={props.title} onChange={props.handleInputChange}/>
-    <br />
-    Description: <input type="text" name="description" value={props.description} onChange={props.handleInputChange}/>
-    <br />
-    Email: <input type="text" name="email" value={props.email} onChange={props.handleInputChange}/>
-    <br />
-    Price: <input type="text" name="price" value={props.price} onChange={props.handleInputChange}/>
-    <br />
-    Image: <input type="file" onChange={props.handleFileChange} />
-    <br />
-    <button type="submit" name="file" onClick={props.createImageUrl}>Submit</button>
-  </form>
+  <div>
+    <span className={styles.upload}>Upload Your Art Here!</span>
+    <form className={styles.form}>
+      Title: <input type="text" name="title" value={props.title} onChange={props.handleInputChange}/>
+      <br />
+      Description: <input type="text" name="description" value={props.description} onChange={props.handleInputChange}/>
+      <br />
+      Email: <input type="text" name="email" value={props.email} onChange={props.handleInputChange}/>
+      <br />
+      Price: <input type="text" name="price" value={props.price} onChange={props.handleInputChange}/>
+      <br />
+      Image: <input type="file" onChange={props.handleFileChange} />
+      <br />
+      <button type="submit" name="file" onClick={props.createImageUrl}>Submit</button>
+    </form>
+  </div>
 )
 
 
